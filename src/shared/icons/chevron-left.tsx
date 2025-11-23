dd5cc0a19c6c571e10372875a9b84481f57d8242
Arsen <arsen.sheripov@gmail.com>
2025-11-23 19:06:39 +0300

    add assets

---
+++ src/shared/icons/chevron-left.tsx
@@ -0,0 +1,6 @@
+import { FC, SVGProps } from 'react';
+export const SvgChevron228Left: FC<SVGProps<SVGSVGElement>> = ({ color = 'currentColor', ...props }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={9} height={14} fill="none" viewBox="0 0 9 14" {...props}>
+    <path stroke="#42567A" strokeWidth={2} d="m7.664.707-6.25 6.25 6.25 6.25" />
+  </svg>
+);
